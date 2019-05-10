5cbef42584962dc6bff97176a795fb4aa05184a0
liusonghuang <huangliusong1994@gmail.com>
2019-05-10 10:25:44 +0800

    (LNode*)malloc(sizeof(LNode));

--- chapter2.3/chapter2.3/main.cpp
+++ chapter2.3/chapter2.3/main.cpp
@@ -12,7 +12,7 @@
 typedef int  ElemEype;
 typedef struct LNode{
     ElemEype data;
-    struct Lnode *next;
+    struct LNode *next;
 }LNode,*LinkList;
 //此行的LNode是结构体struct LNode的一个别名
 //*LinkList也是结构体struct LNode的一个别名
@@ -21,6 +21,53 @@ typedef struct LNode{
 
 //头插法新建链表
 LinkList CreateList1(LinkList &L){
-    
+    LNode *s;
+    int x;
+    L=(LinkList)malloc(sizeof(LNode));
+    L->next=NULL;
+    scanf("%d",&x);
+    while (x!=9999) {
+        s=(LNode*)malloc(sizeof(LNode));
+        s->data=x;
+        s->next=L->next;
+        L->next=s;
+        scanf("%d",&x);
+    }
     return L;
 }
+
+
+LinkList CreateList2(LinkList &L){
+    int x;
+    L=(LinkList)malloc(sizeof(LNode));
+    LNode *s,*r=L;
+    scanf("%d",&x);
+    while (x!=9999) {
+        s=(LNode*)malloc(sizeof(LNode));
+        s->data=x;
+        r->next=s;
+        r=s;//指向新的表尾节点
+        scanf("%d",&x);
+    }
+    r->next=NULL;
+    return L;
+}
+
+//n按序号查找节点值
+LNode *GetElem(LinkList L,int i){
+    int j=1;
+    LNode *p=L->next;
+    if (i==0) {
+        return L;
+    }
+    if (i<1) {
+        return NULL;
+    }
+    while (p&&j<i) {
+        p=p->next;
+        j++;
+    }
+    return p;
+}
+
+//
