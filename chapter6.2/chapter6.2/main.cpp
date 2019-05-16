a2cda02e0a4735022f6a7cccbe421c09b9d89863
liusonghuang <huangliusong1994@gmail.com>
2019-05-16 10:34:06 +0800

    插入树

--- chapter6.2/chapter6.2/main.cpp
+++ chapter6.2/chapter6.2/main.cpp
@@ -1,15 +1,24 @@
-//
-//  main.cpp
-//  chapter6.2
-//
-//  Created by liusong huang on 2019/5/16.
-//  Copyright © 2019 liusong huang. All rights reserved.
-//
+#include <stdio.h>
+#include <stdlib.h>
 
-#include <iostream>
+typedef int  KeyType;
+typedef struct BSTNode {
+    KeyType key;
+    struct BSTNode *lchild,*rchild;
+}BSTNode,*BiTree;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+
+
+int BST_Insert(BiTree &T,KeyType k){
+    if (NULL==T) {
+        T=(BiTree)malloc(sizeof(BSTNode));
+        T->key=k;
+        T->lchild=T->rchild=NULL;
+        return 1;
+    }else if(k==T->key)
+        return 0;
+    else if(k<T->key)
+        return BST_Insert(T->lchild, k);
+    else
+        return BST_Insert(T->rchild, k);
 }
