de0faedc3a650ff3f55d5ccbb8c466215711cdf2
liusonghuang <huangliusong1994@gmail.com>
2019-05-24 10:47:26 +0800

    🤮图的遍历✈️✈️✈️✈️✈️

--- chapter7.2/chapter7.2/main.cpp
+++ chapter7.2/chapter7.2/main.cpp
@@ -1,15 +1,6 @@
-//
-//  main.cpp
-//  chapter7.2
-//
-//  Created by liusong huang on 2019/5/24.
-//  Copyright © 2019 liusong huang. All rights reserved.
-//
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc/malloc.h>
+#include <string.h>
 
-#include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
-}
