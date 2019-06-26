6615a75531ba9b350167e407abf0682efaf96ba5
liusonghuang <huangliusong1994@gmail.com>
2019-06-26 17:13:25 +0800

    提交

--- chapter7.4/chapter7.4/main.cpp
+++ chapter7.4/chapter7.4/main.cpp
@@ -1,42 +1,145 @@
-
-#include <iostream>
-#include <stdlib.h>
-#include <stdio.h>
-
-#define OK 1
-#define ERROR 0
-#define OVERFLOW 0
-#define ElemType int
-#define MAXQSIZE 6
-
-
+#include<stdio.h>
+#include<iostream>
+#include<stdlib.h>
+#include<string.h>
 using namespace std;
+#define maxsize 6
 
 
-typedef struct
-{
-    ElemType *base;
+typedef struct node{
+    int data[maxsize];
     int front;
     int rear;
-    int tag = 0;
-}SqQueue;
+    int tag;
+}seq;
 
 
-//tag t队列初始化
-int init_queue(SqQueue &sq){
-    sq.base=(ElemType*)malloc(MAXQSIZE*sizeof(ElemType));
-    if (!sq.base) {
-        exit(OVERFLOW);
+//init
+void init(seq *q){
+    q->front=0;
+    q->rear=0;
+    q->tag=0;
+}
+
+int empty(seq q){
+    if (q.front==q.rear&&q.tag==0) {
+        return 1;
     }
-    sq.front=sq.rear=0;
-    return OK;
+    else
+        return 0;
+}
+
+// 入队操作
+int insert(seq *q,int e){
+    if (q->front==q->rear&&q->tag==1) {
+        cout<<"队列满了";
+        return 1;
+    }else{
+        q->data[q->rear]=e;
+        q->rear=q->rear+1;
+        q->tag=1;
+        return 1;
+    }
+}
+
+//out queue
+int del(seq *q,int *e){
+    if (empty(*q)) {
+        cout<<"队列是空队列";
+        return 0;
+    }else{
+        *e=q->data[q->front];
+        q->front=q->front+1;
+        q->tag=0;
+        return 1;
+    }
 }
 
 
-//tag d入队
-
-int main(){
-    SqQueue sq;
-    init_queue(sq);
-    printf("init success!\n");
+//dispaly queue
+int dis(seq q)
+{
+    int i;
+    if(empty(q))
+    {
+        return 0;
+    }
+    if(q.front<q.rear)
+    {    for(i=q.front;i<=q.rear;i++)
+        cout<<q.data[i]<<" ";
+    }
+    else
+    {
+        for(i=q.front;i<=q.rear+maxsize;i++)
+            cout<<q.data[i%maxsize]<<" ";
+    }
+    cout<<endl;
+    return 0;
 }
+
+int get(seq q,int *e)
+{
+    if(q.front==q.rear)
+        return 0;
+    else
+    {
+        *e=q.data[q.front];
+        return 1;
+        
+    }
+    
+}
+void clear(seq *q)
+{
+    q->front=q->rear=0;
+}
+
+
+int main()
+{
+    seq q;
+    int x;
+    init(&q);
+    cout<<"1入队！"<<endl;
+    insert(&q,1);
+    
+    cout<<"2入队！"<<endl;
+    insert(&q,2);
+    
+    cout<<"3入队！"<<endl;
+    insert(&q,3);
+    
+    cout<<"4入队！"<<endl;
+    insert(&q,4);
+    
+    cout<<"1出队！"<<endl;
+    del(&q,&x);
+    cout<<"队列首元素为："<<x<<endl;
+    
+    cout<<"2出队！"<<endl;
+    del(&q,&x);
+    cout<<"队列首元素为："<<x<<endl;
+    
+    cout<<"3出队！"<<endl;
+    del(&q,&x);
+    cout<<"队列首元素为："<<x<<endl;
+    
+    cout<<"5入队！"<<endl;
+    insert(&q,5);
+    
+    cout<<"6入队！"<<endl;
+    insert(&q,6);
+    
+    cout<<"7入队！"<<endl;
+    insert(&q,7);
+    
+    printf("剩余队列元素为：");
+    if(!empty(q))
+    {
+        for(int i=q.front;i<q.rear;i++)
+        {
+            cout<<q.data[i]<<" ";
+        }
+    }
+}
+
